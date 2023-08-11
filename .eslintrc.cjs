777a22302c7f869c7076c00152f2263f29dff291
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-11 14:56:57 +0800

    config(lint): configure linting for react
    Copies mostly from https://github.com/vitejs/vite/blob/9322f1b00b85bdd704930415494a5bff29b52fac/packages/create-vite/template-react-ts/.eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,27 @@
 module.exports = {
 	"root": true,
+	"env": { "browser": true, "es2020": true },
 	"extends": [
 		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/recommended-type-checked",
+		"plugin:react-hooks/recommended",
+		"plugin:react/recommended",
+		"plugin:react/jsx-runtime",
 	],
+	"ignorePatterns": ["dist", ".eslintrc.cjs"],
 	"parser": "@typescript-eslint/parser",
-	"plugins": ["@typescript-eslint"],
 	"parserOptions": {
-		"sourceType": "module",
-		"ecmaVersion": 2020,
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: ['./tsconfig.json', './tsconfig.node.json'],
+		tsconfigRootDir: __dirname,
 	},
-	"env": {
-		"browser": true,
-		"es2017": true,
-		"node": true
+	"settings": {
+		"react": {
+			"version": "detect"
+		}
 	},
+	"plugins": ["react-refresh"],
 	"rules": {
 		"max-lines": "warn",
 		"indent": [
@@ -32,13 +39,21 @@ module.exports = {
 			"error",
 			"double"
 		],
-		"quote-props": ["warn", "always"],
+		"quote-props": ["error", "always"],
 		"require-await": "error",
 		"semi": [
 			"error",
 			"always"
 		],
 		"no-unused-vars": "off",
+		"space-before-function-paren": ["error", "never"],
+		"object-curly-spacing": ["error", "always"],
+
+		// React
+		"react-refresh/only-export-components": [
+			"warn",
+			{ "allowConstantExport": true },
+		],
 
 		// typescript-eslint
 		"@typescript-eslint/no-unused-vars": [
@@ -55,7 +70,5 @@ module.exports = {
 		],
 		"@typescript-eslint/explicit-function-return-type": "error",
 
-		"space-before-function-paren": ["error", "never"],
-		"object-curly-spacing": ["error", "always"],
-	}
-}
+	},
+};
