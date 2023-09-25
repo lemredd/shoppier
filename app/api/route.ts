ad248d248de7308894a77b4ec1111664084799bc
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 11:54:26 +0800

    feat(api): fetch on given `API_URL`

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from "next/server";
 
-export default function GET(): ReturnType<typeof NextResponse["json"]> | Response {
-	return NextResponse.json({})
+export async function GET(): Promise<ReturnType<typeof NextResponse["json"]> | Response> {
+	const data = await fetch(`${process.env.API_URL}/users`)
+		.then(res => res.json())
+		.then(data => data as Record<string, any>)
+		.catch(console.error);
+	return NextResponse.json(data);
 }
