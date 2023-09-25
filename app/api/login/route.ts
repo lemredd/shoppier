584048befd36f22ef231603b39d6fd28c2576060
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 13:18:55 +0800

    chore(api): get login fields from form data

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -4,5 +4,8 @@ import { API_URL } from "@api/lib/constants";
 
 export async function POST(request: Request): Promise<ReturnType<typeof NextResponse["json"]> | Response> {
 	const data = await request.formData();
+	const username = data.get("username"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
+	const password = data.get("password"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
+
 	return NextResponse.json(Object.fromEntries(data));
 }
