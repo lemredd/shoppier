46a84285dd5aa647116c2920cb968cb84e2cee30
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 18:07:38 +0800

    feat(api): return empty products if `!keyword`

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -6,11 +6,12 @@ import { API_URL } from "@api/lib/constants";
 
 export async function GET(request: Request): Promise<EndpointResponse> {
 	const url = new URL(request.url);
-	const keyword = url.searchParams.get("keyword") || "";
+	const keyword = url.searchParams.get("keyword");
+
+	if (!keyword) return NextResponse.json({ "products": [] });
 
 	const response = await fetch(`${API_URL}/products/search?q=${keyword}`)
 		.then(res => res.json())
 		.then(data => data as Record<string, any>);
-
 	return NextResponse.json(response);
 }
