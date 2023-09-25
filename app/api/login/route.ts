054812ec6d8adf12a8c1389771aaadfd4e29fa18
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 13:46:42 +0800

    fix(api): extract `login_response` data properly

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -8,7 +8,7 @@ export async function POST(request: Request): Promise<ReturnType<typeof NextResp
 	const password = data.get("password"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 
 	const login_response = 
-		fetch(`${API_URL}/auth/login`, {
+		await fetch(`${API_URL}/auth/login`, {
 			"method": "POST",
 			"headers": { "Content-Type": "application/json" },
 			"body": JSON.stringify({
@@ -17,7 +17,8 @@ export async function POST(request: Request): Promise<ReturnType<typeof NextResp
 				// expiresInMins: 60, // optional
 			})
 		})
-			.then(res => res.json()) as Record<string, any>;
+			.then(res => res.json())
+			.then(data => data as Record<string, any>);
 	const is_valid_credential = Boolean(login_response.id);
 
 	if (!is_valid_credential) return NextResponse.json({ "status": 422, "message": String(login_response.message) });
