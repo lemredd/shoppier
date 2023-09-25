5e385241b8b9474acc86e20e5be0bccbb095fe85
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 22:46:01 +0800

    fix(component): type `Product` and `ProductsList`

--- app/search/Results.tsx
+++ app/search/Results.tsx
@@ -6,11 +6,34 @@ interface Props {
 	keyword: string
 }
 
+interface Product extends Record<string, any> {
+	id: number
+	title: string
+	description: string
+	price: number
+	discountPercentage: number
+	rating: number
+	stock: number
+	brand: string
+	category: string
+	thumbnail: string
+	images: Array<string>
+}
+
+
+interface ProductsList extends Record<string, any> {
+	products: Product[]
+	total: number
+	skip: number
+	limit: number
+}
+
+
 export default function Results({ keyword }: Props): React.ReactElement {
-	const [products, set_products] = useState<Record<string, any>[]>([]);
+	const [products, set_products] = useState<Product[]>([]);
 	
 	useEffect(() => {
-		async function search_products<T extends Record<string, any>>(): Promise<T>{
+		async function search_products<T extends ProductsList>(): Promise<T>{
 			//TODO: handle possible errors!
 			//i.e. example in https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#fetching-data-on-the-server-with-fetch
 			const response =  await fetch(`/api/products/search?keyword=${keyword}`);
@@ -20,13 +43,13 @@ export default function Results({ keyword }: Props): React.ReactElement {
 		}
 
 		search_products()
-			.then(result => set_products(result.products as Record<string, any>[]))
+			.then(result => set_products(result.products))
 			.catch(console.error);
 	}, [keyword]);
 	
 	return (
 		<ul className="results">
-			{products.map((product: Record<string, any>) => (
+			{products.map(product => (
 				<li key={product.id}>{product.title}</li>
 			))}
 		</ul>
