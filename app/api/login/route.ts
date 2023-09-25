fa00186e845c5b2e477498d5f2a385fd79fccd95
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 13:05:07 +0800

    feat(api): decode request body

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -2,10 +2,9 @@ import { NextResponse } from "next/server";
 
 import { API_URL } from "@api/lib/constants";
 
-export async function GET(): Promise<ReturnType<typeof NextResponse["json"]> | Response> {
-	const data = await fetch(`${API_URL}/users`)
-		.then(res => res.json())
-		.then(data => data as Record<string, any>)
-		.catch(console.error);
+export async function POST(request: Request): Promise<ReturnType<typeof NextResponse["json"]> | Response> {
+	const reader = request.body?.getReader();
+	const { value } = await reader!.read();
+	const data = JSON.parse(new TextDecoder().decode(value)) as Record<string, any>;
 	return NextResponse.json(data);
 }
