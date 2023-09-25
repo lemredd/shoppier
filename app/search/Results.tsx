7fec9f499e30bb472af0a67616393aa953c195aa
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 22:11:10 +0800

    chore(component): make `products` state

--- app/search/Results.tsx
+++ app/search/Results.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 interface Props {
 	keyword: string
 }
+
 export default function Results({ keyword }: Props): React.ReactElement {
+	const [products, set_products] = useState<Record<string, any>[]>([]);
+	
+	
 	return (
 		<ul className="results">
 			{products.map((product: Record<string, any>) => (
