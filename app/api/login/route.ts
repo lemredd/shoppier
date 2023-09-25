efc5310a9a579cc623493b5d66484755326ce4f4
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 13:56:16 +0800

    chore(page): add todos

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -7,20 +7,19 @@ export async function POST(request: Request): Promise<ReturnType<typeof NextResp
 	const username = data.get("username"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 	const password = data.get("password"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 
-	const login_response = 
-		await fetch(`${API_URL}/auth/login`, {
-			"method": "POST",
-			"headers": { "Content-Type": "application/json" },
-			"body": JSON.stringify({
-				username,
-				password,
-				// expiresInMins: 60, // optional
-			})
+	const login_response = await fetch(`${API_URL}/auth/login`, {
+		"method": "POST",
+		"headers": { "Content-Type": "application/json" },
+		"body": JSON.stringify({
+			username,
+			password,
+			// expiresInMins: 60, // optional
 		})
-			.then(res => res.json())
-			.then(data => data as Record<string, any>);
+	})
+		.then(res => res.json())
+		.then(data => data as Record<string, any>);
 	const is_valid_credential = Boolean(login_response.id);
 
 	if (!is_valid_credential) return NextResponse.json({ "status": 422, "message": String(login_response.message) });
 	return NextResponse.json(login_response);
-;}
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,7 +7,8 @@ export default function Page(): React.ReactNode {
 		event.preventDefault();
 
 		function handle_data(data: Record<string, any>): void {
-			if (data.status === 422) console.error(data.message);
+			if (data.status === 422) console.error(data.message); // TODO: show error on UI
+			else localStorage.setItem("user", JSON.stringify(data)); // TODO: `router.push` to home
 		}
 
 		const form_data = new FormData(event.target as HTMLFormElement);
