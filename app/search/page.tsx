c5208ccd668c7fdc22a44a9197b6b4694b284866
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 16:21:17 +0800

    chore(page): make function to `search_products`

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,6 +2,15 @@
 
 import { PageProps } from "@/.next/types/app/search/page";
 
+async function search_products<T extends Record<string, any>>(keyword: string): Promise<T> {
+	const products = await fetch(`/api/products/search?keyword=${keyword}`)
+		.then(res => res.json())
+		.then(data => data as T)
+		.catch(console.error);
+
+	return products as T;
+}
+
 export default function Page({ searchParams }: PageProps): React.ReactNode {
 	const keyword = searchParams.get("keyword");
 	function submit(): void {
