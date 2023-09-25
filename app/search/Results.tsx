9209210f6be303c1e1eb393d7d11c416dc7ef318
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 22:12:11 +0800

    refactor(component): search products on client
    Allows for uniformity in fetch URL

--- app/search/Results.tsx
+++ app/search/Results.tsx
@@ -9,6 +9,18 @@ interface Props {
 export default function Results({ keyword }: Props): React.ReactElement {
 	const [products, set_products] = useState<Record<string, any>[]>([]);
 	
+	useEffect(() => {
+		async function search_products<T extends Record<string, any>>(): Promise<T>{
+			const response =  await fetch(`/api/products/search?keyword=${keyword}`);
+			const result = await response.json() as T;
+
+			return result;
+		}
+
+		search_products()
+			.then(result => set_products(result.products as Record<string, any>[]))
+			.catch(console.error);
+	}, [keyword]);
 	
 	return (
 		<ul className="results">
