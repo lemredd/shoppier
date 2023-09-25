82d2aab574bfdeb0e5325517e5173d4083fb177a
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 21:21:23 +0800

    chore(component): handle submission of search form

--- app/search/SearchBar.tsx
+++ app/search/SearchBar.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import type { FormEvent } from "react";
+
 interface Props {
 	keyword: string | string[] | undefined
 }
 
 export default function SearchBar({ keyword }: Props): React.ReactElement {
+	function handle_submit(event: FormEvent): void {
+	}
 	return (
-		<form>
-			<input type="text" name="keyword" value={keyword} />
+		<form onSubmit={handle_submit}>
+			<input type="text" name="keyword" defaultValue={keyword} />
 		</form>
 	);
 }
