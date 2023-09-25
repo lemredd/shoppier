63f9a59b628198a1f4222dc750974d63b031a7ae
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 19:03:56 +0800

    chore(page): reorder interface declaration

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,16 +1,16 @@
 import { PageProps } from "@/.next/types/app/search/page";
 
+interface SearchPageProps extends PageProps {
+	params: Record<"slug", string>
+	searchParams: Record<string, string | string[] | undefined>
+}
+
 async function search_products<T extends Record<string, any>>(keyword: string): Promise<T>{
 	// TODO: handle possible errors!
 	// i.e. example in https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#fetching-data-on-the-server-with-fetch
 	return await fetch(`${process.env.API_URL}/products/search?q=${keyword}`)
 		.then(res => res.json())
 		.then(data => data as T);
-}
-
-interface SearchPageProps extends PageProps {
-	params: Record<"slug", string>
-	searchParams: Record<string, string | string[] | undefined>
 }
 
 export default async function Page({ searchParams }: SearchPageProps): Promise<React.ReactElement> {
