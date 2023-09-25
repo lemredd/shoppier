c8e2dc71b9f3489a1ff9f2336b6faac1552f2d66
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 14:08:33 +0800

    feat(api): look existing user before registering

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -6,6 +6,12 @@ export async function POST(request: Request): Promise<ReturnType<typeof NextResp
 	const data = await request.formData();
 	const username = data.get("username"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 	const password = data.get("password"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
+
+	const found_user = await fetch(`${API_URL}/users?q=${String(username)}`)
+		.then(res => res.json())
+		.then(data => data as Record<string, any>);
+	if (found_user.length) return NextResponse.json({ "status": 400, "message": "User exists." });
+	
 	const register_response = await fetch(`${API_URL}/users/add`, {
 		"method": "POST",
 		"headers": { "Content-Type": "application/json" },
@@ -17,6 +23,5 @@ export async function POST(request: Request): Promise<ReturnType<typeof NextResp
 	})
 		.then(res => res.json())
 		.then(data => data as Record<string, any>);
-
 	return NextResponse.json(register_response);
 }
