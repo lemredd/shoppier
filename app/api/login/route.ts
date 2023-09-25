d9f5441eecb84d31b8877b0e449984fc43ea39c8
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-25 13:08:54 +0800

    refactor(api): use `formData` instead

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,8 +3,6 @@ import { NextResponse } from "next/server";
 import { API_URL } from "@api/lib/constants";
 
 export async function POST(request: Request): Promise<ReturnType<typeof NextResponse["json"]> | Response> {
-	const reader = request.body?.getReader();
-	const { value } = await reader!.read();
-	const data = JSON.parse(new TextDecoder().decode(value)) as Record<string, any>;
-	return NextResponse.json(data);
+	const data = await request.formData();
+	return NextResponse.json(Object.fromEntries(data));
 }
