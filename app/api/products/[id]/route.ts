52e0230a4af51d91b41826e667b07056cda83d7d
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 17:43:32 +0800

    feat(api): make `DELETE` route for product

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -26,7 +26,26 @@ export async function GET(_request: Request, context: Context): Promise<Endpoint
 
 export async function PATCH(request: Request, context: Context): Promise<EndpointResponse> {
 	const { id } = context.params;
-	console.log("from server:", id, isNaN(id));
+	if (isNaN(id)) return respond_if_invalid_id();
+
+	const form_data = await request.formData();
+	const entries = Object.fromEntries(form_data) as RequiredProductModificationProps;// TODO: validate!!!;
+
+	const data = await fetch(`${API_URL}/products/${id}`, {
+		"method": "PATCH",
+		"headers": { "content-type": "application/json" },
+		// TODO: allow inclusion of images. Before that, store these mock data in a real database
+		"body": JSON.stringify({ ...entries } satisfies RequiredProductModificationProps)
+	})
+		.then(res => res.json())
+		.then(data => data as Product)
+		.catch(console.error);
+	
+	return NextResponse.json(data);
+}
+
+export async function DELETE(request: Request, context: Context): Promise<EndpointResponse> {
+	const { id } = context.params;
 	if (isNaN(id)) return respond_if_invalid_id();
 
 	const form_data = await request.formData();
