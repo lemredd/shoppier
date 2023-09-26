e00c2420bb572a8e7d658dd433c5152dc30b8881
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 17:55:57 +0800

    fix(api): correct API URL for deleting product

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -48,15 +48,7 @@ export async function DELETE(request: Request, context: Context): Promise<Endpoi
 	const { id } = context.params;
 	if (isNaN(id)) return respond_if_invalid_id();
 
-	const form_data = await request.formData();
-	const entries = Object.fromEntries(form_data) as RequiredProductModificationProps;// TODO: validate!!!;
-
-	const data = await fetch(`${API_URL}/products/${id}`, {
-		"method": "PATCH",
-		"headers": { "content-type": "application/json" },
-		// TODO: allow inclusion of images. Before that, store these mock data in a real database
-		"body": JSON.stringify({ ...entries } satisfies RequiredProductModificationProps)
-	})
+	const data = await fetch(`${API_URL}/products/${id}`, { "method": "DELETE" })
 		.then(res => res.json())
 		.then(data => data as Product)
 		.catch(console.error);
