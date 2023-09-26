2c183c9b23fd3c4fa7a01ecf00cbda371103e11a
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 23:24:09 +0800

    refactor(type): make `Product` schemas

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -11,6 +11,21 @@ export interface Product extends Record<string, any> {
 	thumbnail: string
 	images: Array<string>
 }
+import { object, infer as extract, string, number, array } from "zod";
+
+export const product_schema = object({
+	"id": number(),
+	"title": string(),
+	"description": string(),
+	"price": number(),
+	"discountPercentage": number(),
+	"rating": number(),
+	"stock": number(),
+	"brand": string(),
+	"category": string(),
+	"thumbnail": string().url(),
+	"images": array(string().url())
+});
 
 export interface ProductsList extends Record<string, any> {
 	products: Product[]
@@ -19,3 +34,9 @@ export interface ProductsList extends Record<string, any> {
 	limit: number
 }
 
+const product_list_schema = object({
+	"product": array(product_schema),
+	"total": number(),
+	"skip": number(),
+	"limit": number()
+});
