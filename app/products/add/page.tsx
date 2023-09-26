971ac012812edadd9bac5976f11e054c1a0255c1
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 12:50:24 +0800

    feat(page): handle form submission
    Copies from `app/login/page.tsx`

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import { FormEvent } from "react";
+
+import type { Product } from "@/app/lib/types";
+
 export default function Page(): React.ReactNode {
+	function submit(event: FormEvent): void {
+		event.preventDefault();
+
+		function handle_data(data: Product): void {
+			if (data.status === 422) return console.error(data.message); // TODO: show error on UI
+			console.log(data);
+		}
+
+		const form_data = new FormData(event.target as HTMLFormElement);
+		fetch("/api/products", {
+			"method": "POST",
+			"body": form_data
+		})
+			.then(response => response.json())
+			.then(handle_data)
+			.catch(console.error);
+	}
+
 	return (
-		<>
-			add product
-		</>
+		<form onSubmit={submit} method="POST">
+			<input type="text" name="username" />
+			<input type="password" name="password" />
+			<input type="submit" value="Submit" />
+		</form>
 	);
 }
+
