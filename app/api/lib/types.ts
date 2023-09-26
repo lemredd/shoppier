d86a3b3e376016ec2dfdbb6c97e6e7717bb32f7c
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-27 00:39:18 +0800

    fix(type): refine schema properties as number
    Properties affected:
    - `price`
    - `stock`

--- app/api/lib/types.ts
+++ app/api/lib/types.ts
@@ -1,5 +1,5 @@
-import type { NextResponse } from "next/server";
-import { infer as extract } from "zod";
+import { NextResponse } from "next/server";
+import { infer as extract, string } from "zod";
 
 import { product_schema } from "@/app/lib/types";
 
@@ -11,6 +11,9 @@ export const product_creation_schema = product_schema.pick({
 	"price": true,
 	"stock": true,
 	"description": true
+}).extend({
+	"price": string().refine(value => !isNaN(Number(value)), "Please enter a number for this field."),
+	"stock": string().refine(value => !isNaN(Number(value)), "Please enter a number for this field.")
 });
 
 export type ProductCreationFormEntries = extract<typeof product_creation_schema>;
