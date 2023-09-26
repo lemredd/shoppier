b03c699c863cb3ddec65ddc55e59a3e79a5b10a8
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 12:28:24 +0800

    chore(api): make `RequiredProductCreationProps`

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,8 +1,17 @@
 import { NextResponse } from "next/server";
 
+import type { Product } from "@/app/lib/types";
 import type { EndpointResponse } from "@api/lib/types";
 
 import { API_URL } from "@api/lib/constants";
+
+interface RequiredProductCreationProps extends Partial<Product> {
+	title: string
+	brand: string
+	price: number
+	stock: number
+	description: string
+}
 
 export async function POST(request: Request): Promise<EndpointResponse> {
 	const form_data = await request.formData();
