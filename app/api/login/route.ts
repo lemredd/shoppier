edb6c9d97be0faf2bcdcf7d1369b109f1d3309c8
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 22:40:29 +0800

    chore(api): make `login_schema`

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,13 +1,17 @@
+import { object, string } from "zod";
 import { NextResponse } from "next/server";
 
 import type { EndpointResponse } from "@api/lib/types";
 
 import { API_URL } from "@api/lib/constants";
 
+const login_schema = object({
+	"username": string(),
+	"password": string().min(8)
+});
+
 export async function POST(request: Request): Promise<EndpointResponse> {
 	const data = await request.formData();
-	const username = data.get("username"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
-	const password = data.get("password"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 
 	const login_response = await fetch(`${API_URL}/auth/login`, {
 		"method": "POST",
