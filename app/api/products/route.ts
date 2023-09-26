9594736bb451f0c26b9627eb91cb87f24310a924
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 23:58:20 +0800

    feat(api): validate entries in product creation

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,13 +1,23 @@
 import { NextResponse } from "next/server";
 
 import type { Product } from "@/app/lib/types";
-import type { EndpointResponse, ProductCreationFormEntries } from "@api/lib/types";
+import {
+	product_creation_schema,
+	type EndpointResponse,
+	type ProductCreationFormEntries
+} from "@api/lib/types";
 
 import { API_URL } from "@api/lib/constants";
 
 export async function POST(request: Request): Promise<EndpointResponse> {
 	const form_data = await request.formData();
 	const entries = Object.fromEntries(form_data) as ProductCreationFormEntries;
+
+	try {
+		product_creation_schema.parse(entries);
+	} catch(e) {
+		return NextResponse.json(e, { "status": 422 });
+	}
 
 	const data = await fetch(`${API_URL}/products/add`, {
 		"method": "POST",
