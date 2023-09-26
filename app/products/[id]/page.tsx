6da1caac64435cc86172f4e8ed674c9bfcc28354
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 11:19:46 +0800

    feat(page): make `/product/[id]` page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,13 @@
+interface PageProps {
+	params: Record<string, string>
+}
+
+export default function Page({ params }: PageProps): React.ReactNode {
+	const { id } = params;
+	
+	return (
+		<main>
+			 page
+		</main>
+	);
+}
