9d041fa47d35aa617a8bad1834cf3919fb29d7f8
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 23:40:57 +0800

    refactor(types): pick `Product` properties

--- app/api/lib/types.ts
+++ app/api/lib/types.ts
@@ -1,13 +1,16 @@
 import type { NextResponse } from "next/server";
+import { infer as extract } from "zod";
 
-import type { Product } from "@/app/lib/types";
+import { product_schema } from "@/app/lib/types";
 
 export type EndpointResponse = ReturnType<typeof NextResponse["json"]> | Response
 
-export interface RequiredProductCreationProps extends Partial<Product> {
-	title: string
-	brand: string
-	price: number
-	stock: number
-	description: string
-}
+const product_creation_schema = product_schema.pick({
+	"title": true,
+	"brand": true,
+	"price": true,
+	"stock": true,
+	"description": true
+});
+
+export type ProductCreationFormEntries = extract<typeof product_creation_schema>;
