f3b484399fb77487abf16cf174b415aa80f6ea86
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-26 17:25:48 +0800

    fix(api): correct user search URL

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -9,7 +9,7 @@ export async function POST(request: Request): Promise<EndpointResponse> {
 	const username = data.get("username"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 	const password = data.get("password"); // TODO: validate manually or with 3rd party validator (i.e. Zod, Valibot)
 
-	const found_user = await fetch(`${API_URL}/users?q=${String(username)}`)
+	const { "users": found_user } = await fetch(`${API_URL}/users/search?q=${String(username)}`)
 		.then(res => res.json())
 		.then(data => data as Record<string, any>);
 	if (found_user.length) return NextResponse.json("User exists.", { "status": 422 });
