39f257c7da1ad01ab8860c5f483452a3ac68053b
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-28 01:54:28 +0800

    chore(api): make schema for register form

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,8 +1,21 @@
+import { infer as extract, object, string } from "zod";
 import { NextResponse } from "next/server";
 
+import type { Prisma } from "@prisma/client";
 import type { EndpointResponse } from "@api/lib/types";
 
 import { SERVER_URL } from "@api/lib/constants";
+
+// Before you ask why, this is because exported types from the generated `Prisma` namespace aren't recognized.
+// Tested in both Neovim and VSCode/ium. Can't figure out why it just won't recognize them.
+// TODO: find a fix for this. Should just `Omit` `username, password` from `Prisma.UserCreateInput`
+const register_form_entries_schema = object({
+	"username": string(),
+	"password": string(),
+	"confirm_password": string()
+});
+
+type RegisterFormEntries = extract<typeof register_form_entries_schema>
 
 export async function POST(request: Request): Promise<EndpointResponse> {
 	const data = await request.formData();
