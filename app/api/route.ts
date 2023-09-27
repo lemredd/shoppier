69ed8681c922ae63ff7ad839a912b39e13ebf6a8
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-27 23:51:10 +0800

    feat(api): `skip` and `take` by `searchParams`

--- app/api/route.ts
+++ app/api/route.ts
@@ -7,8 +7,8 @@ import { user_operator } from "@api/lib/operator";
 export async function GET(request: NextRequest): Promise<EndpointResponse> {
 	const { skip, limit } = Object.fromEntries(request.nextUrl.searchParams);
 	const data = await user_operator.findMany({
-		"skip": 0,
-		"take": 3
+		"skip": !isNaN(Number(skip)) ? Number(skip) : 0,
+		"take": !isNaN(Number(limit)) ? Number(limit) : 10
 	});
 	return NextResponse.json(data);
 }
