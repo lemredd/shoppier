0064075dbec5e5c8e76952738941001502de6bb7
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-28 02:11:35 +0800

    fix(api): validate matching passwords

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -8,11 +8,14 @@ import { SERVER_URL } from "@api/lib/constants";
 
 // Before you ask why, this is because exported types from the generated `Prisma` namespace aren't recognized.
 // Tested in both Neovim and VSCode/ium. Can't figure out why it just won't recognize them.
-// TODO: find a fix for this. Should just `Omit` `username, password` from `Prisma.UserCreateInput`
+// TODO: find a fix for this. Should just `Pick` `username, password` from `Prisma.UserCreateInput`
 const register_form_entries_schema = object({
 	"username": string(),
 	"password": string().min(8, "Password should be at least 8 characters long."),
 	"confirm_password": string().min(8, "Password should be at least 8 characters long.")
+}).refine(({ password, confirm_password }) => password === confirm_password, {
+	"message": "Passwords don't match.",
+	"path": ["password", "confirm_password"]
 });
 
 type RegisterFormEntries = extract<typeof register_form_entries_schema>
