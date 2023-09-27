2920a8d4d1ae8e4364c50e713ed43e29b6d4a5ed
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-27 23:50:25 +0800

    feat(api): get URL `searchParams`
    params to consider:
    - `skip`
    - `limit`

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 import type { EndpointResponse } from "@api/lib/types";
 
 import { user_operator } from "@api/lib/operator";
 
-export async function GET(): Promise<EndpointResponse> {
+export async function GET(request: NextRequest): Promise<EndpointResponse> {
+	const { skip, limit } = Object.fromEntries(request.nextUrl.searchParams);
 	const data = await user_operator.findMany({
 		"skip": 0,
 		"take": 3
