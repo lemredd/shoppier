fd6a6530aa945ade3fa7a06190d9fc50ce1218a9
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-27 21:55:51 +0800

    feat(seed): batch product creation by 10
    Limit is 100 as per fake API.
    Recurse to prevent overload if `?limit=100` or somewhere `> 10`

--- db/seed/product.ts
+++ db/seed/product.ts
@@ -17,19 +17,22 @@ interface FakeProduct {
 	images: Array<string>
 }
 
-export default async function seed_products(prisma: PrismaClient): Promise<void> {
-	const { "products": fake_products } = await fetch(`${FAKE_API_URL}/products`)
+export default async function seed_products(prisma: PrismaClient, skip = 0): Promise<void> {
+	if (skip > 100) return;
+	const { "products": fake_products } = await fetch(`${FAKE_API_URL}/products?limit=10&skip=${skip}`)
 		.then(res => res.json())
 		.then(data => data as { products: FakeProduct[] });
-		
+	
 	fake_products.forEach(product => {
 		const product_to_create = product as Partial<FakeProduct>;
 		delete product_to_create.id;
 		prisma.product.create({
 			"data": { ...product_to_create as typeof product, "images": product.images.join(",") }
-		}).catch(console.error);
+		})
+			.catch(console.error);
 	});
 
-	const all_users = await prisma.user.findMany({ "include": { "address": true } });
-	console.log(all_users);
+	seed_products(prisma, skip + 10).catch(console.error);
+	const all_products = await prisma.product.findMany();
+	console.log(all_products);
 }
