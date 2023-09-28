c3470fe33d6700bef760a5c3431ad9c04feb89a6
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-29 02:23:44 +0800

    feat(api): include products `count`

--- app/api/products/search/route.ts
+++ app/api/products/search/route.ts
@@ -1,16 +1,19 @@
 import { NextResponse } from "next/server";
 
+import type { ProductsList } from "@/app/lib/types";
 import type { EndpointResponse } from "@api/lib/types";
 
 import { product_operator } from "@api/lib/operator";
 
+// TODO: paginate
 export async function GET(request: Request): Promise<EndpointResponse> {
+	// TODO: require `skip` and `limit` search params
 	const url = new URL(request.url);
 	const keyword = url.searchParams.get("keyword");
 
 	if (!keyword) return NextResponse.json({ "products": [] });
 
-	// TODO: paginate and include count
+	// TODO: `skip` and `offset`
 	const response = product_operator.findMany({ "where": {
 		"OR": [
 			{ "title": { "contains": keyword } },
@@ -19,7 +22,12 @@ export async function GET(request: Request): Promise<EndpointResponse> {
 			{ "category": { "contains": keyword } }
 		]
 	} }).then(
-		products => NextResponse.json(products)
+		products => NextResponse.json({
+			"products": products.map(product => ({ ...product, "images": product.images?.split(",") ?? [] })),
+			"skip": 0, // TODO: `skip` and `offset`
+			"limit": 10, // TODO: `skip` and `offset`
+			"total": products.length
+		} satisfies ProductsList)
 	).catch(
 		e => NextResponse.json(e, { "status": 422 })
 	);
