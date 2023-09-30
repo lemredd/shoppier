ecd7d91a92e2da5a9848816c89b6bd59cf40cef9
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-30 23:49:27 +0800

    fix(helper): use `SERVER_URL`
    This getter is now running on server components.

--- app/products/lib/get_product.ts
+++ app/products/lib/get_product.ts
@@ -1,7 +1,9 @@
-import { Product } from "@/app/lib/types";
+import type { Product } from "@/app/lib/types";
+
+import { SERVER_URL } from "@/app/lib/constants";
 
 export default async function get_product<T extends Product>(id: number): Promise<T> {
-	return await fetch(`/api/products/${id}`)
+	return await fetch(`${SERVER_URL}/api/products/${id}`)
 		.then(res => res.json())
 		.then(data => data as T);
 }
