d6d4d10b48afc632ea38b037392e77a7fdaa4a8f
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-30 22:16:03 +0800

    chore(page): prepare cart when viewing product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,11 +1,14 @@
+import get_user_cart from "@/app/lib/get_user_cart";
+
 import Product from "./Product";
 
 interface PageProps {
 	params: Record<"id", number>
 }
 
-export default function Page({ params }: PageProps): React.ReactNode {
+export default async function Page({ params }: PageProps): Promise<React.ReactElement> {
 	const { id } = params;
+	const cart = await get_user_cart();
 	
 	return (
 		<>
