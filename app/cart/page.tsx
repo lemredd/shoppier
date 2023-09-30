8a43b7dd1659fe3a47a6ced6b14047f801397933
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-30 11:38:25 +0800

    fix(page): get user id from cookie instead
    Reason: `localStorage` or `document.cookies` is not available in server
    side components

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,11 @@
+import { cookies } from "next/headers";
+
+import type { Cart } from "@prisma/client";
+
 import { SERVER_URL } from "@app/lib/constants";
 
-async function get_user_cart(user_id: number): Promise<any> {
+async function get_user_cart(user_id: number): Promise<Cart> {
+	// TODO: consider anonymous cart
 	return await fetch(`${SERVER_URL}/api/cart`, {
 		"method": "POST",
 		"headers": { "content-type": "application/json" },
@@ -8,16 +13,13 @@ async function get_user_cart(user_id: number): Promise<any> {
 	}).then(
 		res => res.json()
 	).then(
-		data => data as Record<string, any>
+		data => data as Cart
 	);
 }
 export default async function Page(): Promise<React.ReactElement> {
-	// TODO: find a way to preload
-	if (typeof window !== "undefined") {
-		const current_user = JSON.parse(localStorage.getItem("user") as string);
-		const data = await get_user_cart(Number(current_user.id));
-		console.log(data);
-	}
+	const auth = cookies().get("auth");
+	const cart = auth?.value ? await get_user_cart(Number(auth.value)) : undefined;
+	console.log(cart);
 	return (
 		<main>
 			root page
