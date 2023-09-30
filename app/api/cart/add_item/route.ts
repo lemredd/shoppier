c09d71bdf3cdab5e295d8a398f991af01cf24cb1
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-30 23:03:26 +0800

    feat(api): add cart item

--- app/api/cart/add_item/route.ts
+++ app/api/cart/add_item/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 import type { EndpointResponse } from "@api/lib/types";
 
-import { cart_operator } from "@api/lib/operator";
+import { cart_item_operator } from "@api/lib/operator";
 
 const form_data_schema = object({
 	"id": string().refine(value => !isNaN(Number(value))),
@@ -23,15 +23,14 @@ export async function POST(request: NextRequest): Promise<EndpointResponse> {
 		return NextResponse.json(e, { "status": 422 });
 	}
 
-	// TODO: handle error if `create` fails
-	// TODO: consider anonymous carts
-	async function create_cart(): Promise<EndpointResponse> {
-		return NextResponse.json(await cart_operator.create({ "data": {
-			"user_id": decoded_body.user_id,
-		} }));
-	}
-	const response = cart_operator.findUniqueOrThrow({ "where": { "user_id": decoded_body.user_id } })
-		.then(cart =>  NextResponse.json(cart))
-		.catch(create_cart);
+	const response = cart_item_operator.create({ "data": {
+		"cartId": Number(entries.id),
+		"productId": Number(entries.product_id),
+		"quantity": Number(entries.quantity)
+	} }).then(
+		cart_item => NextResponse.json(cart_item)
+	).catch(
+		e => NextResponse.json(e, { "status": 422 })
+	);
 	return response;
 }
