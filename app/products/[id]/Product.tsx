d7dc5e4757f86bf5d9b0c58580d4e29ebb602a5f
Lem Redd <lmoa.jhdp@gmail.com>
2023-09-30 23:48:08 +0800

    refactor(component): use `AddToCartForm`

--- app/products/[id]/Product.tsx
+++ app/products/[id]/Product.tsx
@@ -1,12 +1,8 @@
-"use client";
-
-import { FormEvent, useEffect, useState } from "react";
-
 import { Cart } from "@prisma/client";
 
-import type { Product } from "@/app/lib/types";
-
 import get_product from "@app/products/lib/get_product";
+
+import AddToCartForm from "@app/products/[id]/components/AddToCartForm";
 
 interface Props {
 	id: number
@@ -18,49 +14,15 @@ export const preload = (id: number): void => {
 	void get_product(id);
 };
 
-export default function ProductDetails({ id, cart }: Props): React.ReactNode {
-	const [product, set_product] = useState<Product>();
-	const [is_adding_item, set_is_adding_item] = useState<boolean>();
-
-	useEffect(() => {
-		get_product(id)
-			.then(set_product)
-			.catch(console.error);
-	}, [id]);
-
-	function add_to_cart(event: FormEvent): void {
-		event.preventDefault();
-		const form_data = new FormData(event.target as HTMLFormElement);
-
-		fetch("/api/cart/add_item", {
-			"method": "POST",
-			"body": form_data
-		}).then(console.log).catch(console.error);
-	}
-
+export default async function ProductDetails({ id, cart }: Props): Promise<React.ReactElement> {
 	return (
 		<>
 			{/*
 			* TODO: wrap with semantic HTML
 			* TODO: Display the rest of product details
 			*/}
-			{product?.title}
-			<button type="button" onClick={(): void => set_is_adding_item(true)}>Add to cart</button>
-			<dialog open={is_adding_item}>
-				<form onSubmit={add_to_cart} method="POST">
-					<input type="hidden" name="id" defaultValue={cart.id} />
-					<input type="hidden" name="product_id" defaultValue={id} />
-					<input type="number" name="quantity" defaultValue={1} />
-					<input type="submit" value="Add to Cart" />
-				</form>
-				
-				<button
-					type="button"
-					onClick={(): void => set_is_adding_item(false)}
-				>
-					cancel
-				</button>
-			</dialog>
+			{data.title}
+			<AddToCartForm {...{ id, cart }} />
 		</>
 	);
 }
