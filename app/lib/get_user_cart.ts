aef6e48bcc20ac383cc0b79fce5d935347912581
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-02 10:00:00 +0800

    fix(helper): get `auth` cookie instead

--- app/lib/get_user_cart.ts
+++ app/lib/get_user_cart.ts
@@ -9,12 +9,11 @@ interface UserCart extends Cart {
 }
 
 export default async function get_user_cart(): Promise<UserCart> {
-	// TODO: consider anonymous cart
-	const user_id = Number(cookies().get("auth")?.value);
+	const auth_token = cookies().get("auth")?.value;
 	return await fetch(`${SERVER_URL}/api/cart`, {
 		"method": "POST",
 		"headers": { "content-type": "application/json" },
-		"body": JSON.stringify({ user_id })
+		"body": JSON.stringify({ auth_token })
 	}).then(
 		res => res.json()
 	).then(
