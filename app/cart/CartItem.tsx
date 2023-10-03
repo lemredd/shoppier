1863020217980a402d366932fb9ec00ea3993671
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-03 21:46:48 +0800

    feat(component): make `EditCartItemForm`

--- app/cart/CartItem.tsx
+++ app/cart/CartItem.tsx
@@ -8,13 +8,26 @@ interface Props {
 	item: CartProduct
 }
 
+function EditCartItemForm({ id, quantity }: Pick<CartProduct, "id" | "quantity">): React.ReactElement {
+	function update(): void {
+		console.log;
+	}
+
+	return (
+		<form>
+			<label>Quantity: <input type="number" defaultValue={quantity} /></label>
+			<input type="submit" value="Update" onClick={update} />
+		</form>
+	);
+}
+
 export default function CartItem({ item }: Props): React.ReactElement {
 	const [is_editing, set_is_editing] = useState(false);
 
 	return (
 		<li>
 			<h1>{item.id}</h1>
-			
+			<EditCartItemForm id={item.id} quantity={item.quantity} />
 		</li>
 	);
 }
