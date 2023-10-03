a78a031f469439e390d8017bcbaa17dd37661e2b
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-03 21:41:12 +0800

    chore(component): make `is_editing` state

--- app/cart/CartItem.tsx
+++ app/cart/CartItem.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { CartProduct } from "@prisma/client";
+import { useState } from "react";
+
+import type { CartProduct } from "@prisma/client";
 
 interface Props {
 	item: CartProduct
 }
 
 export default function CartItem({ item }: Props): React.ReactElement {
+	const [is_editing, set_is_editing] = useState(false);
+
 	return (
-		<li>{item.id}</li>
+		<li>
+			<h1>{item.id}</h1>
+			
+		</li>
 	);
 }
 
