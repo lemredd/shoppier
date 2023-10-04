8db6cd78350ed28ae13931e19da540a7c9c6f53a
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-04 21:24:18 +0800

    fix(component): handle storing anonymous cart

--- app/cart/Cart.tsx
+++ app/cart/Cart.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { UserCart } from "@app/lib/get_user_cart";
 
 import CartItem from "./CartItem";
@@ -6,6 +8,13 @@ interface Props {
 	cart: UserCart
 }
 export default function Cart({ cart }: Props): React.ReactElement {
+	const is_anonymous = "id" in cart === false;
+	if (is_anonymous) {
+		cart = JSON.parse(localStorage.getItem("cart")!) as UserCart
+			|| { "products": [] };
+		localStorage.setItem("cart", JSON.stringify(cart));
+	}
+
 	return (
 		<>
 			{cart.products.length && cart.products.map(product => (
