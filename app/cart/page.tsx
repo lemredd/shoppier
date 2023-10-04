8e941f54e3954ddbe651280325955c3b22b96115
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-04 20:59:08 +0800

    refactor(page): use `Cart`

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,13 +1,7 @@
 import get_user_cart from "@app/lib/get_user_cart";
-import CartItem from "./CartItem";
+import Cart from "./Cart";
 
 export default async function Page(): Promise<React.ReactElement> {
 	const cart = await get_user_cart();
-	return (
-		<main>
-			{cart.products && cart.products.map(product => (
-				<CartItem key={product.id} item={product} />
-			)) || "You have no items in your cart yet."}
-		</main>
-	);
+	return <Cart cart={cart} />;
 }
