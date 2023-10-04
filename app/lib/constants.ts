364d0e705d5b39342085b4c05053a5601cf887e9
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-04 21:31:11 +0800

    refactor(constant): generalize error message
    Name of error message: `NO_AUTH_TOKEN_PROVIDED_MESSAGE`

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,4 @@
 export const { SERVER_URL } = process.env;
+
+export const NO_AUTH_TOKEN_PROVIDED_MESSAGE =
+	"You are not currently logged in. Items you add in your cart will be stored in the browser.";
