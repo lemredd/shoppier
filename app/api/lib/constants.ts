9e1510a23683120c8af5ae4a68212b0b01b217ac
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-04 21:47:49 +0800

    refactor(constant): relocate error message
    Reason: `NO_AUTH_TOKEN_PROVIDED_MESSAGE` will only be used in route
    handlers

---
+++ app/api/lib/constants.ts
@@ -0,0 +1,3 @@
+export const NO_AUTH_TOKEN_PROVIDED_MESSAGE =
+	"You are not currently logged in. Items you add in your cart will be stored in the browser.";
+
--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,4 +1,2 @@
 export const { SERVER_URL } = process.env;
 
-export const NO_AUTH_TOKEN_PROVIDED_MESSAGE =
-	"You are not currently logged in. Items you add in your cart will be stored in the browser.";
