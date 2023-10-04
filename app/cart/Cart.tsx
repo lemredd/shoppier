601e0dfa50413c0d64362192c27188d88065ef03
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-04 21:33:24 +0800

    refactor(component): use and alias error message
    Error message used: `NO_AUTH_TOKEN_PROVIDED_MESSAGE` ->
    `ANONYMOUS_CART_WARN_MESSAGE`

--- app/cart/Cart.tsx
+++ app/cart/Cart.tsx
@@ -3,6 +3,7 @@
 import { UserCart } from "@app/lib/get_user_cart";
 
 import CartItem from "./CartItem";
+import { NO_AUTH_TOKEN_PROVIDED_MESSAGE as ANONYMOUS_CART_WARN_MESSAGE } from "@app/lib/constants";
 
 interface Props {
 	cart: UserCart
@@ -18,7 +19,7 @@ export default function Cart({ cart }: Props): React.ReactElement {
 	const { products } = cart;
 	return (
 		<>
-			{is_anonymous && <p>You are not currently logged in. Items you add in your cart will be stored in the browser.</p>}
+			{is_anonymous && <p>{ANONYMOUS_CART_WARN_MESSAGE}</p>}
 			{products.length && products.map(product => (
 				<CartItem key={product.id} item={product} />
 			)) || "You have no items in your cart yet."}
