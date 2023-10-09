41851e5f4fd409d16fdd01bbd8755694c8a9d1e6
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-09 10:00:00 +0800

    refactor(helper): add `mutate_products` callback

--- app/lib/access_anonymous_cart.ts
+++ app/lib/access_anonymous_cart.ts
@@ -1,10 +1,13 @@
 import { UserCart } from "@app/lib/types";
 
-export default function access_anonymous_cart<T = UserCart>(): T {
+export default function access_anonymous_cart<T = UserCart>(
+	mutate_products?: (cart: T) => void
+): T {
 	const cart = JSON.parse(localStorage.getItem("cart")!) as T
-		|| { "products": [] };
+		|| { "products": [] } as T;
+	if (mutate_products) mutate_products(cart);
 	localStorage.setItem("cart", JSON.stringify(cart));
 
-	return cart as T;
+	return cart;
 }
 
