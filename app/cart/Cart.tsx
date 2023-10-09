5b812dcadfccba8eaf56b6395a04ff1adfefb9ed
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-09 09:00:00 +0800

    fix(component): extract issue if `is_anonymous`

--- app/cart/Cart.tsx
+++ app/cart/Cart.tsx
@@ -11,10 +11,11 @@ interface Props {
 }
 export default function Cart({ cart }: Props): React.ReactElement {
 	const is_anonymous = "id" in cart === false;
-	const { issues } = cart as unknown as ZodError; // TODO: this seems ugly. fix it.
-	const [anonymous_cart_issue] = issues;
+	let anonymous_cart_issue;
 
 	if (is_anonymous) {
+		const { issues } = is_anonymous && cart as unknown as ZodError; // TODO: this seems ugly. fix it.
+		anonymous_cart_issue = issues[0];
 		cart = JSON.parse(localStorage.getItem("cart")!) as UserCart
 			|| { "products": [] };
 		localStorage.setItem("cart", JSON.stringify(cart));
@@ -23,7 +24,7 @@ export default function Cart({ cart }: Props): React.ReactElement {
 	const { products } = cart;
 	return (
 		<>
-			{is_anonymous && <p>{anonymous_cart_issue.message}</p>}
+			{is_anonymous && <p>{anonymous_cart_issue?.message}</p>}
 			{products.length && products.map(product => (
 				<CartItem key={product.productId} item={product} />
 			)) || "You have no items in your cart yet."}
