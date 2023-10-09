11f66a2f824ad73e936af9f5991169f8e2ab960a
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-09 15:49:10 +0800

    config(test): configure cypress initially

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+	"component": {
+		"devServer": {
+			"framework": "next",
+			"bundler": "webpack",
+		},
+		"specPattern": [
+			"./cypress/component/**/*.test.{ts,tsx}",
+			"./app/**/*.test.{ts,tsx}",
+		]
+	},
+});
