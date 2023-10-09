7f7419eab4e2af75f7f32d23c0a12f24a29b1631
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-09 16:13:19 +0800

    test(component): expect default value

--- cypress/component/Counter.test.tsx
+++ cypress/component/Counter.test.tsx
@@ -2,6 +2,11 @@ import Counter from "./Counter";
 
 describe("Component: <Counter />", () => {
 	it("mounts", () => {
-		cy.mount(<Counter />)
-	})
-})
+		cy.mount(<Counter />);
+	});
+
+	it("defaults `count` to 0", () => {
+		cy.mount(<Counter />);
+		cy.get("button").should("have.text", "Count: 0");
+	});
+
