e420c3a2a4f639c2925606dc99637ae201d037b5
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-09 21:24:32 +0800

    test(api): expect response to be a array

--- app/api/route.test.ts
+++ app/api/route.test.ts
@@ -2,4 +2,12 @@ describe("Route: `/api`", () => {
 	it("requests", () => {
 		cy.request("/api");
 	});
+
+	it("responds with list of ..." /* For now, this lists users */, () => {
+		cy.request("/api").then(response => {
+			// TODO: make route depend on a test environment database
+			expect(response.status).to.equal(200);
+			expect(response.body).to.be.a("array");
+		});
+	});
 });
