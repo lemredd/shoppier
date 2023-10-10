a28cc00c906447226a7352b7cca79cd1ea656a67
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-10 23:16:36 +0800

    test(component): get by `aria-label`
    As influenced by principles of `testing-library`

--- app/products/[id]/components/AddToCartForm.test.tsx
+++ app/products/[id]/components/AddToCartForm.test.tsx
@@ -3,7 +3,7 @@ import Component from "./AddToCartForm";
 describe("Component: `AddToCartForm`", () => {
 	it("shows dialog when `add-to-cart-btn` is clicked", () => {
 		cy.mount(<Component cart={{ "products": [] }} id={1} />);
-		cy.get("#add-to-cart-btn").click();
+		cy.get("button[aria-label='Add to cart']").click();
 		cy.get("dialog").should("exist");
 	});
 });
