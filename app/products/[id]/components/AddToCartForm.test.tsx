05a23736dbc88eba3f12df1808b4270a92404a9a
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-11 22:08:57 +0800

    test(component): expect `dialog` to `be.visible`

--- app/products/[id]/components/AddToCartForm.test.tsx
+++ app/products/[id]/components/AddToCartForm.test.tsx
@@ -2,9 +2,10 @@ import Component from "./AddToCartForm";
 
 // TODO(E2E): test adding cart item to both authenticated (`fetch`) and anonymous (`localStorage`) users
 describe("Component: `AddToCartForm`", () => {
-	it("shows dialog when `add-to-cart-btn` is clicked", () => {
+	it("shows dialog when 'Add to cart' button is clicked", () => {
 		cy.mount(<Component cart={{ "products": [] }} id={1} />);
 		cy.get("button[aria-label='Add to cart']").click();
-		cy.get("dialog").should("exist");
+		cy.get("dialog").should("be.visible");
+	});
 	});
 });
