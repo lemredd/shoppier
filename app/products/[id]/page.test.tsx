9ddcea0b4c22b8536230cd58f9dcf3900f8a253a
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-12 11:38:28 +0800

    test(page): expect product added to anonymous cart

--- app/products/[id]/page.test.tsx
+++ app/products/[id]/page.test.tsx
@@ -1,9 +1,18 @@
 const env = { "PORT": 7357 };
 
 describe("Page: `/products/[id]`", { env }, () => {
-	it("loads product", () => {
+	it("adds product to anonymous cart", () => {
+		// TODO: make fixture for `product` and intercept
 		cy.visit("/products/1");
-		cy.get("button[aria-label='Add to cart']").should("exist");
+		cy.get("button[aria-label='Add to cart']").click();
+		cy.get("form input[type=submit]").click();
+
+		cy.getAllLocalStorage().then(({ "http://localhost:7357": localhost }) => {
+			expect(JSON.parse(String(localhost.cart))).not.to.throw;
+			const anonymous_cart = JSON.parse(String(localhost.cart)) as Record<string, unknown>;
+
+			expect(anonymous_cart.products).to.have.length(1);
+		});
 	});
 });
 
