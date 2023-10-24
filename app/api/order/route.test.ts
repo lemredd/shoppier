3cd9443eeb7ddcde7dfdcdd57b1aaff1976930d5
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-24 13:46:11 +0800

    test(api): expect `401` if `auth_token` is invalid

--- app/api/order/route.test.ts
+++ app/api/order/route.test.ts
@@ -1,6 +1,14 @@
-describe("Route `/api` - Unauthorized Access", () => {
+describe("Route `/api/order` - Unauthorized Access", () => {
 	it("responds with unauthorized status", () => {
 		cy.getCookie("auth").should("be.null");
+		cy.request({
+			"url": "/api/order",
+			"failOnStatusCode": false
+		}).then(response => {
+			expect(response.status).to.equal(401);
+		});
+
+		cy.setCookie("auth", "foobarbaz");
 		cy.request({
 			"url": "/api/order",
 			"failOnStatusCode": false
