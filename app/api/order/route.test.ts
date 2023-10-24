92e02dc878042a08361fc90ec0ff26c3fa195800
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-24 10:28:21 +0800

    test(api): expect `auth` cookie to be null

--- app/api/order/route.test.ts
+++ app/api/order/route.test.ts
@@ -1,6 +1,10 @@
 describe("Route `/api` - Unauthorized Access", () => {
 	it("responds with unauthorized status", () => {
-		cy.request("/api/order").then(response => {
+		cy.getCookie("auth").should("be.null");
+		cy.request({
+			"url": "/api/order",
+			"failOnStatusCode": false
+		}).then(response => {
 			expect(response.status).to.equal(401);
 		});
 	});
