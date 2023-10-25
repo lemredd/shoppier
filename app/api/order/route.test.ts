68070b7310b98fc19210107f04bde5d963dc8a08
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-25 17:50:31 +0800

    test(api): expect response to be `OK` and array

--- app/api/order/route.test.ts
+++ app/api/order/route.test.ts
@@ -23,5 +23,12 @@ describe("Route `/api/order` - Order Operations", () => {
 		// TODO: make setting cookie unmanual (this is currently whiteboxed)
 		// TODO: make separate database for test environment (currently testing on dev environment database)
 		cy.setCookie("auth", "Chaim_McDermott@dana.io_1698226523787");
+		cy.request({
+			"url": "/api/order",
+			"failOnStatusCode": false
+		}).then(response => {
+			expect(response.status).to.equal(200);
+			expect(response.body).to.be.a("array");
+		});
 	});
 });
