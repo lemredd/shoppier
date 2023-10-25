ec5126c6081648f21367534fae88514af916008c
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-25 17:49:47 +0800

    test(api): set cookie manually
    Currently, the cookie value set is data from dev environment database.

--- app/api/order/route.test.ts
+++ app/api/order/route.test.ts
@@ -18,3 +18,10 @@ describe("Route `/api/order` - Unauthorized Access", () => {
 	});
 });
 
+describe("Route `/api/order` - Order Operations", () => {
+	it("lists all orders of authenticated user", () => {
+		// TODO: make setting cookie unmanual (this is currently whiteboxed)
+		// TODO: make separate database for test environment (currently testing on dev environment database)
+		cy.setCookie("auth", "Chaim_McDermott@dana.io_1698226523787");
+	});
+});
