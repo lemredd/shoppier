3bb06bf4cc0c0286c0818e697232f17c32fb393d
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-25 17:51:11 +0800

    feat(api): find orders by user

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -19,7 +19,9 @@ export async function GET(request: NextRequest): Promise<EndpointResponse> {
 
 	if (found_user_or_error instanceof Error) return NextResponse.json(found_user_or_error, { "status": 401 });
 
-	const response = order_operator.findMany().then(orders => NextResponse.json(orders));
+	const response = order_operator.findMany({
+		"where": { "user_id": found_user_or_error.id }
+	}).then(orders => NextResponse.json(orders));
 
 	return response;
 }
