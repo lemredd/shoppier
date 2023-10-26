a80b86c0e2268fc86829f4395b184bcb1f2ffde6
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-26 21:47:35 +0800

    test(api): expect error if invalid password

--- app/api/login/route.test.ts
+++ app/api/login/route.test.ts
@@ -28,6 +28,19 @@ describe("Route `/api/login` - Validation and Errors", () => {
 			expect(response.status).to.equal(422);
 		});
 
+		// Existing user but invalid password
+		body.set("username_or_email", "TEST_DATA@email.com");
+		body.set("password", "invalid_password");
+		cy.request({
+			"method": "POST",
+			"url": "/api/login",
+			"headers": { "content-type": "multipart/form-data" },
+			body,
+			"failOnStatusCode": false
+		}).then(response => {
+			expect(response.isOkStatusCode).to.be.false;
+			expect(response.status).to.equal(422);
+		});
 	});
 });
 
