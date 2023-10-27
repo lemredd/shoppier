75487187a26bd1be29e4f335e8926f7cb34b6051
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 12:55:09 +0800

    feat(page): redirect if `auth` cookie exists
    Simply, redirect if user is logged in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,12 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 import LoginForm from "./components/LoginForm";
 
 // TODO: redirect to homepage if `auth` cookie is set
 export default function Page(): React.ReactNode {
+	const auth_token = cookies().get("auth")?.value;
+	if (auth_token) redirect("/");
 
 	return (
 		<LoginForm />
