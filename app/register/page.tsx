48050e9657e778577397119eeb541508b48805eb
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 13:32:15 +0800

    feat(page): redirect if logged in

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,6 +1,12 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 import RegisterForm from "./components/RegisterForm";
 
 export default function Page(): React.ReactNode {
+	const auth_token = cookies().get("auth")?.value;
+	if (auth_token) redirect("/");
+
 	return (
 		<RegisterForm />
 	);
