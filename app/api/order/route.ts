253a2b066277b54b81a35257eed0dbd316cf7633
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 17:46:53 +0800

    feat(api): validate authenticated user

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -28,3 +28,24 @@ export async function GET(request: NextRequest): Promise<EndpointResponse> {
 
 	return response;
 }
+export async function POST(request: NextRequest): Promise<EndpointResponse> {
+	const auth_token = request.cookies.get("auth");
+	if (!auth_token) return NextResponse.json(null, { "status": 401 });
+	const auth_token_inclusions = { "user": {
+		"include": { "cart": {
+			"include": { "products": true }
+		} }
+	} };
+	const found_user_or_error = await auth_token_operator.findUniqueOrThrow({
+		"where": { "value": auth_token.value },
+		"include": auth_token_inclusions
+	}).then(
+		token => token.user
+	).catch(
+		(error: Error) => error
+	);
+	if (found_user_or_error instanceof Error) return NextResponse.json(
+		found_user_or_error,
+		{ "status": 401 }
+	);
+}
