6746d15ff0fcf707efe300476b284eb184454234
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 13:31:58 +0800

    test(page): test `/register` page

---
+++ app/register/page.test.tsx
@@ -0,0 +1,23 @@
+const env = { "PORT": 7357 };
+
+describe("Page: `/register`", { env }, () => {
+	const LOCALHOST_URL = `http://localhost:${env.PORT}`;
+
+	it("redirects if logged in already", () => {
+		cy.setCookie("auth", "TEST_DATA_auth_token");
+
+		cy.visit("/register");
+		cy.url().should("equal",`${LOCALHOST_URL}/`);
+	});
+
+	it("redirects to home after successful login", () => {
+		cy.visit("/login");
+		
+		cy.get("input[name='username_or_email']").type("TEST_DATA@email.com");
+		cy.get("input[name='password']").type("TEST_DATA_password");
+		cy.get("form input[type=submit]").click();
+
+		cy.url().should("equal",`${LOCALHOST_URL}/`);
+	});
+});
+
