4c501ffbb3b01cd64462cb0803aa1d2a081a6721
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 12:54:58 +0800

    refactor(page): use `LoginForm`

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,33 +1,10 @@
-"use client";
 
-import { useRouter } from "next/navigation";
-
-import type { FormEvent } from "react";
+import LoginForm from "./components/LoginForm";
 
 // TODO: redirect to homepage if `auth` cookie is set
 export default function Page(): React.ReactNode {
-	const router = useRouter();
-	function submit(event: FormEvent): void {
-		event.preventDefault();
-
-		function handle_response(response: Response): void {
-			if (response.ok) return router.push("/");
-			console.error(response.body); // TODO: show error on UI
-		}
-		const form_data = new FormData(event.target as HTMLFormElement);
-		fetch("/api/login", {
-			"method": "POST",
-			"body": form_data
-		})
-			.then(handle_response)
-			.catch(console.error);
-	}
 
 	return (
-		<form onSubmit={submit} method="POST">
-			<input type="text" name="username_or_email" />
-			<input type="password" name="password" />
-			<input type="submit" value="Submit" />
-		</form>
+		<LoginForm />
 	);
 }
