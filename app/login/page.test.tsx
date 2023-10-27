43f1e59500fd7edb838cae2622cd041deb8370f5
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 13:23:02 +0800

    test(page): expect redirection after logging in

--- app/login/page.test.tsx
+++ app/login/page.test.tsx
@@ -10,6 +10,14 @@ describe("Page: `/products/[id]`", { env }, () => {
 		cy.url().should("equal",`${LOCALHOST_URL}/`);
 	});
 
+	it("redirects to home after successful login", () => {
+		cy.visit("/login");
+		
+		cy.get("input[name='username_or_email']").type("TEST_DATA@email.com");
+		cy.get("input[name='password']").type("TEST_DATA_password");
+		cy.get("form input[type=submit]").click();
+
+		cy.url().should("equal",`${LOCALHOST_URL}/`);
 	});
 });
 
