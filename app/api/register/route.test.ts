973db30e70dc560e8c96040b7568b56e64802d42
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 10:49:53 +0800

    test(api): expect errors on specific cases
    Can't use `response.body` to expect `ZodError` (`response.body` is empty)

--- app/api/register/route.test.ts
+++ app/api/register/route.test.ts
@@ -11,5 +11,49 @@ describe("Route `/api/register` - Validation and Errors", () => {
 			expect(response.isOkStatusCode).to.be.false;
 		});
 
+		// Invalid email
+		body.set("email", "invalid_email");
+		body.set("password", "password");
+		body.set("password", "password");
+		cy.request({
+			"method": "POST",
+			"url": "/api/register",
+			"headers": { "content-type": "multipart/form-data" },
+			body,
+			"failOnStatusCode": false
+		}).then(response => {
+			expect(response.isOkStatusCode).to.be.false;
+		});
+
+		// Password is less than 8 characters
+		body.set("email", "sample@email.com");
+		body.set("password", "passwor");
+		body.set("password", "passwor");
+		cy.request({
+			"method": "POST",
+			"url": "/api/register",
+			"headers": { "content-type": "multipart/form-data" },
+			body,
+			"failOnStatusCode": false
+		}).then(response => {
+			expect(response.isOkStatusCode).to.be.false;
+		});
+
+		// Passwords don't match
+		body.set("email", "sample@email.com");
+		body.set("password", "password");
+		body.set("password", "password1");
+		cy.request({
+			"method": "POST",
+			"url": "/api/register",
+			"headers": { "content-type": "multipart/form-data" },
+			body,
+			"failOnStatusCode": false
+		}).then(response => {
+			expect(response.isOkStatusCode).to.be.false;
+		});
+	});
+});
+
 	});
 });
