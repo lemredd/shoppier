d668f1fd73316db3a1088dd328f53e486aa58bd3
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-27 13:22:51 +0800

    test(page): expect redirection if logged in

--- app/login/page.test.tsx
+++ app/login/page.test.tsx
@@ -1,11 +1,15 @@
 const env = { "PORT": 7357 };
 
 describe("Page: `/products/[id]`", { env }, () => {
+	const LOCALHOST_URL = `http://localhost:${env.PORT}`;
+
 	it("redirects if logged in already", () => {
 		cy.setCookie("auth", "TEST_DATA_auth_token");
 
 		cy.visit("/login");
-		cy.url().should("equal", "/");
+		cy.url().should("equal",`${LOCALHOST_URL}/`);
+	});
+
 	});
 });
 
