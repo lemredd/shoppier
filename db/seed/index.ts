e602c05960993f2585d87987f15b9fed1634039c
Lem Redd <lmoa.jhdp@gmail.com>
2023-10-30 08:00:00 +0800

    chore(seed): add todo

--- db/seed/index.ts
+++ db/seed/index.ts
@@ -6,6 +6,7 @@ import seed_products from "./product.js";
 const prisma = new PrismaClient();
 
 async function main(): Promise<void> {
+	// TODO: use `faker.js` instead of Fake API
 	await seed_users(prisma);
 	await seed_products(prisma);
 }
